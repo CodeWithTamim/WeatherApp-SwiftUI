d3789f3997eeb9af89314f72c9024b2e35611a85
Tamim Hossain <132823494+CodeWithTamim@users.noreply.github.com>
2024-09-01 13:59:00 +0600

    Initial Commit

---
+++ Siwft UI/Siwft_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Siwft_UIApp.swift
+//  Siwft UI
+//
+//  Created by Tamim Hossain on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Siwft_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
